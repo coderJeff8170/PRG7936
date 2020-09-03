c6a65166dd661b3255b6a1ab8c02eaae6b578691
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-02 20:47:47 -0400

    lesson one submit

--- L01HandsOn/src/index.js
+++ L01HandsOn/src/index.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const applesPrice = React.createElement('li', {}, '$1.00 per lb');
-// const apples = React.createElement('ul', {}, applesPrice);
-// const orangesPrice = React.createElement('li', {}, '$.70 per lb');
-// const oranges = React.createElement('ul', {}, orangesPrice);
-// const mangosPrice = React.createElement('li', {}, '$2.00 per lb');
-// const mangos = React.createElement('ul', {}, mangosPrice);
-
-// ReactDOM.render(
-//     React.createElement(
-//         'ul',
-//         {},
-//         React.createElement('li', {}, 'apples', apples),
-//         React.createElement('li', {}, 'oranges', oranges),
-//         React.createElement('li', {}, 'mangos', mangos)
-//     ),
-//     document.getElementById('root')
-// );
-
 const title = React.createElement('h1', {id: 'title'}, 'These are a few of Jeff\'s favourite things');
 //colors:
 
