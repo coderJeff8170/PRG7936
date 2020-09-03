6090102050934614e8f256745770ad50e9c38c03
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-03 14:33:40 -0400

    lesson 2 module completed ramping up to functional components...

--- react-lesson-two/src/index.js
+++ react-lesson-two/src/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
+const item1 = 'apples';
+const item1Price = '$1.00 per lb';
+const item2 = 'oranges';
+const item2Price = '$.70 per lb';
+const item3 = 'mangos';
+const item3Price = '$2.00 per lb';
+
+const groceryList = (item1, item1Price, item2, item2Price, item3, item3Price) => {
+return (
+    <ul>
+    <li>{item1}</li>
+    <ul>
+        <li>{item1Price}</li>
+    </ul>
+    <li>{item2}</li>
+    <ul>
+        <li>{item2Price}</li>
+    </ul>
+    <li>{item3}</li>
+    <ul>
+        <li>{item3Price}</li>
+    </ul>
+</ul>
+)};
+
 ReactDOM.render(
-<ul>
-    <li>Apples</li>
-    <ul>
-        <li>$1.00 per lb</li>
-    </ul>
-    <li>Oranges</li>
-    <ul>
-        <li>$.70 per lb</li>
-    </ul>
-    <li>Mangos</li>
-    <ul>
-        <li>$2.00 per lb</li>
-    </ul>
-</ul>,
+groceryList(item1, item1Price, item2, item2Price, item3, item3Price),
 document.getElementById('root')
 );
