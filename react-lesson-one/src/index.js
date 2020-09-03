3791ef7b0dc5be2ea6f765c42fe64f15976baf60
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-02 20:49:08 -0400

    module 1 practice files

--- react-lesson-one/src/index.js
+++ react-lesson-one/src/index.js
@@ -1,67 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const applesPrice = React.createElement('li', {}, '$1.00 per lb');
-// const apples = React.createElement('ul', {}, applesPrice);
-// const orangesPrice = React.createElement('li', {}, '$.70 per lb');
-// const oranges = React.createElement('ul', {}, orangesPrice);
-// const mangosPrice = React.createElement('li', {}, '$2.00 per lb');
-// const mangos = React.createElement('ul', {}, mangosPrice);
-
-// ReactDOM.render(
-//     React.createElement(
-//         'ul',
-//         {},
-//         React.createElement('li', {}, 'apples', apples),
-//         React.createElement('li', {}, 'oranges', oranges),
-//         React.createElement('li', {}, 'mangos', mangos)
-//     ),
-//     document.getElementById('root')
-// );
-
-const title = React.createElement('h1', {id: 'title'}, 'These are a few of Jeff\'s favourite things');
-//colors:
-
-const colorList = React.createElement('ol', {className: 'faveColors'},
-                        React.createElement('li', {className: 'color'}, 'orange'), 
-                        React.createElement('li', {className: 'color'}, 'blue'),
-                        React.createElement('li', {className: 'color'}, 'green')
-    );
-const favColors = React.createElement('li', {className: 'listHeader'}, 'Favorite Colors:', colorList);
-//music:
-const musicList = React.createElement('ol', {className: 'faveMusic'},
-                        React.createElement('li', {className: 'band'}, 'Cardiacs'), 
-                        React.createElement('li', {className: 'band'}, 'Peter Gabriel'),
-                        React.createElement('li', {className: 'band'}, 'Yes')
-    );
-const favMusic = React.createElement('li', {className: 'listHeader'}, 'Favorite Music:', musicList);
-//food:
-const foodList = React.createElement('ol', {className: 'faveFoods'},
-                        React.createElement('li', {className: 'food'}, 'Black Forest Gateaux'), 
-                        React.createElement('li', {className: 'food'}, 'Eclairs'),
-                        React.createElement('li', {className: 'food'}, 'Sushi')
-    );
-const favFood = React.createElement('li', {className: 'listHeader'}, 'Favorite Foods:', foodList);
-//sites:
-const siteList = React.createElement('ol', {className: 'list'},
-                        React.createElement('li', {className: 'site'}, 
-                            React.createElement('a', {href: 'https://www.paintedbyjeff.com', target: '_blank'}, 'My Ancient Website')), 
-                        React.createElement('li', {className: 'site'}, 
-                            React.createElement('a', {href: 'https://codepen.io/coderJeff8170', target: '_blank'}, 'My CodePen')),
-                        React.createElement('li', {className: 'site'}, 
-                            React.createElement('a', {href: 'https://www.w3schools.com/', target: '_blank'}, 'My goto for truncated Dev reference'))
-    );
-const favSites = React.createElement('li', {className: 'listHeader'}, 'Favorite Websites:', siteList);
-
-
-
-const list = React.createElement('ul', {id: 'faves list'}, favColors, favMusic, favFood, favSites);
+const applesPrice = React.createElement('li', {}, '$1.00 per lb');
+const apples = React.createElement('ul', {}, applesPrice);
+const orangesPrice = React.createElement('li', {}, '$.70 per lb');
+const oranges = React.createElement('ul', {}, orangesPrice);
+const mangosPrice = React.createElement('li', {}, '$2.00 per lb');
+const mangos = React.createElement('ul', {}, mangosPrice);
 
 ReactDOM.render(
-    React.createElement('div',
-     {},
-     title,
-     list
+    React.createElement(
+        'ul',
+        {},
+        React.createElement('li', {}, 'apples', apples),
+        React.createElement('li', {}, 'oranges', oranges),
+        React.createElement('li', {}, 'mangos', mangos)
     ),
     document.getElementById('root')
 );
+
