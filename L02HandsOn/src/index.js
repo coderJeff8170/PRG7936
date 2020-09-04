dd5e439f1a11073ceddc2d2de0fa9fa1e116978f
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-04 02:34:43 -0400

    started lesson 2 hands on

--- L02HandsOn/src/index.js
+++ L02HandsOn/src/index.js
@@ -1,1 +1,21 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const stateInfo = (state, population, capital) => {
+    return(
+        <table>
+            <tr>
+            <th>{state}</th>
+            <th>{population}</th>
+            <th>{capital}</th>
+            </tr>
+        </table>
+        
+    )
+
+};
+
+ReactDOM.render(
+    stateInfo('State', 'Population', 'Capital'),
+    document.getElementById('root')
+);
