18eda61fafa95685510ce1f3227ac6737ddd3125
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-04 14:48:13 -0400

    supercharged the jsx functional components in the table needs some work

--- L02HandsOn/src/index.js
+++ L02HandsOn/src/index.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const stateInfo = (state, population, capital) => {
+// sorry Shaun, I got carried away...
+let data = [
+    {state: 'Idaho', pop: null, capital: 'Boise'},
+    {state: 'Tennessee', pop: 6.651, capital: 'Nashville'},
+    {state: 'Maine', pop: 1.331, capital: 'Augusta'},
+    {state: 'Wisconsin', pop: 5.779, capital: 'Madison'},
+    ];
+
+
+const tableHead = () => {
     return(
-        <table>
-            <tr>
-            <th>{state}</th>
-            <th>{population}</th>
-            <th>{capital}</th>
+        <thead>
+            <tr style={{fontWeight: 'bold'}}>
+                <td>State</td>
+                <td>Population</td>
+                <td>Capital</td>
             </tr>
-        </table>
-        
+        </thead>
+
     )
 
+}
+const stateInfo = (state, pop, capital, i) => {
+    return(
+        //<style>{"table{border:1px solid black;}"}</style> -> figure this out
+        //put the thead in here, and then the map function?
+        <tr 
+            key={i}
+            style={i%2==0?{background: '#ddd'}:{background: 'white'}}
+            // style={{background: '#ddd'}}
+        >
+            <td>{state}</td>
+            <td>{pop>0?`${pop} million`:''}</td>
+            <td>{capital}</td>
+        </tr>
+    )
 };
 
 ReactDOM.render(
-    stateInfo('State', 'Population', 'Capital'),
+    <table>
+        {tableHead()}
+        <tbody>
+        {data.map((el, i)=>(stateInfo(el.state, el.pop, el.capital, i)))}
+        </tbody>
+        
+    </table>,
     document.getElementById('root')
 );
