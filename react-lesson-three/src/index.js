9dab331d2c06e82ed6e977cbb2e04106ca7f1c84
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-08 15:07:50 -0400

    halfway through lesson three modules

--- react-lesson-three/src/index.js
+++ react-lesson-three/src/index.js
@@ -1,1 +1,30 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = props => {
+    return(
+        <Employee name="Sophia" age={13} position="MakeupArtist"/>
+    )
+};
+
+const Employee = props => (
+    <ul>
+        <li>{props.name}</li>
+        <li>{props.age}</li>
+        <li>{props.position}</li>
+    </ul>
+);
+
+Employee.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    position: PropTypes.string
+}
+
+
+
+ReactDOM.render(
+<App/>,
+document.getElementById('root')
+);
