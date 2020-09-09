bb19e3d8590b6d1dff41f0f772840fd9275aa46c
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-09 19:12:33 -0400

    futzing around with why css isn't applying styles

--- L02HandsOn/src/index.js
+++ L02HandsOn/src/index.js
@@ -11,26 +11,26 @@ let data = [
     ];
 
 // jsObject styling (after finding what a pain global element styling without a library in React can be):
-const tableStyle = {
-    fontFamily: 'helvetica, arial, sansSerif',
-    width: '500px',
-    textAlign: 'center',
-    borderSpacing: '0'
-}
+// const tableStyle = {
+//     fontFamily: 'helvetica, arial, sansSerif',
+//     width: '500px',
+//     textAlign: 'center',
+//     borderSpacing: '0'
+// }
 
-const cellStyle = {
-    border: '1px solid black',
-    padding: '5px 10px'
-}
+// const cellStyle = {
+//     border: '1px solid black',
+//     padding: '5px 10px'
+// }
 
 // create column heads
 const tableHead = () => {
     return(
         <thead>
             <tr style={{fontWeight: 'bold'}}>
-                <th style = {cellStyle}>State</th>
-                <th style = {cellStyle}>Population</th>
-                <th style = {cellStyle}>Capital</th>
+                <th>State</th>
+                <th>Population</th>
+                <th>Capital</th>
             </tr>
         </thead>
     )
@@ -43,9 +43,9 @@ const tableRow = (state, pop, capital, i) => {
             key={i}
             style={i%2==0?{background: '#ddd'}:{background: 'white'}}
         >
-            <td style = {cellStyle}>{state}</td>
-            <td style = {cellStyle}>{pop>0?`${pop} million`:''}</td>
-            <td style = {cellStyle}>{capital}</td>
+            <td>{state}</td>
+            <td>{pop>0?`${pop} million`:''}</td>
+            <td>{capital}</td>
         </tr>
     )
 };
@@ -54,7 +54,7 @@ const tableRow = (state, pop, capital, i) => {
 // which creates entire table
 const stateInfo = (info) => {
     return(
-        <table style={tableStyle}>
+        <table>
             {tableHead()}
             <tbody>
                 {info.map((el, i)=>(tableRow(el.state, el.pop, el.capital, i)))}
