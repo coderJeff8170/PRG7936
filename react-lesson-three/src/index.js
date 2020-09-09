937fbf4e99b072323c434f3b61c9660ce0f84781
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-09 18:49:22 -0400

    proptypes component learnin lesson three

--- react-lesson-three/src/index.js
+++ react-lesson-three/src/index.js
@@ -3,23 +3,52 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 const App = props => {
-    return(
-        <Employee name="Sophia" age={13} position="MakeupArtist"/>
-    )
+    const user = {
+        name: "Sophia",
+        address: "3939 Woodgreen Terrace",
+        city: "Charlotte",
+        state: "North Carolina",
+        zip: 28205
+
+    };
+    return <User person={user} items="guitar, hoodie, leggings" amount={140}/>;
 };
 
-const Employee = props => (
+const User = props => (
     <ul>
-        <li>{props.name}</li>
-        <li>{props.age}</li>
-        <li>{props.position}</li>
+        <li>Name: {props.person.name}</li>
+            <ul>
+                <li>Address: {props.person.address}</li>
+                <li>City: {props.person.city}</li>
+                <li>State: {props.person.state}</li>
+                <li>Zip: {props.person.zip}</li>
+            </ul>
+        <li>Items: {props.items}</li>
+        <li>Shipped: {String(props.shipped)}</li>
+        <li>Paid: {String(props.paid)}</li>
+        <li>Amount: {props.amount}</li>
     </ul>
 );
 
-Employee.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number.isRequired,
-    position: PropTypes.string
+
+
+User.propTypes = {
+    person: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        address: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired,
+        zip: PropTypes.number.isRequired,
+    }),
+    items: PropTypes.string.isRequired,
+    paid: PropTypes.bool,
+    amount: PropTypes.number.isRequired,
+    shipped: PropTypes.bool
+}
+
+User.defaultProps = {
+    paid: false,
+    shipped: false
 }
 
 
