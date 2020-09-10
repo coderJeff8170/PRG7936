084476f42e67bf1b45eb9fa3fb690402d3c0eb87
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-10 14:08:30 -0400

    using for loops to make transforms

--- react-lesson-three/src/index.js
+++ react-lesson-three/src/index.js
@@ -1,59 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 
-const App = props => {
-    const user = {
-        name: "Sophia",
-        address: "3939 Woodgreen Terrace",
-        city: "Charlotte",
-        state: "North Carolina",
-        zip: 28205
 
-    };
-    return <User person={user} items="guitar, hoodie, leggings" amount={140}/>;
+//okay now make this work with the map function!
+const list = ['bread', 'milk', 'eggs', 'tea', 'liquor'];
+
+const listItems = [];
+
+for (let i=0; i<list.length; i++) {
+    listItems.push(<li key={i}>{list[i]}</li>);
 };
 
-const User = props => (
-    <ul>
-        <li>Name: {props.person.name}</li>
-            <ul>
-                <li>Address: {props.person.address}</li>
-                <li>City: {props.person.city}</li>
-                <li>State: {props.person.state}</li>
-                <li>Zip: {props.person.zip}</li>
-            </ul>
-        <li>Items: {props.items}</li>
-        <li>Shipped: {String(props.shipped)}</li>
-        <li>Paid: {String(props.paid)}</li>
-        <li>Amount: {props.amount}</li>
-    </ul>
-);
+const List = props => <ul>{props.items}</ul>;
 
-
-
-User.propTypes = {
-    person: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        address: PropTypes.string.isRequired,
-        city: PropTypes.string.isRequired,
-        state: PropTypes.string.isRequired,
-        zip: PropTypes.number.isRequired,
-    }),
-    items: PropTypes.string.isRequired,
-    paid: PropTypes.bool,
-    amount: PropTypes.number.isRequired,
-    shipped: PropTypes.bool
-}
-
-User.defaultProps = {
-    paid: false,
-    shipped: false
-}
-
-
+const App = props => {
+    return <List items={listItems}/>
+};
 
 ReactDOM.render(
-<App/>,
+<App />,
 document.getElementById('root')
 );
