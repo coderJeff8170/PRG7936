b0ac44ae1e9bd2743571f4efc2e077f11691c8ac
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-11 14:51:07 -0400

    lesson three full example multiple component rendering with dynamic content

--- react-lesson-three/src/index.js
+++ react-lesson-three/src/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const GroceryList = props => {
+    return (
+        <ul>
+            <li>{props.item1}</li>
+            <li>{props.item2}</li>
+            <li>{props.item3}</li>
+        </ul>
+    );
+}
 
 
-//okay now make this work with the map function!
+
 const List = props => {
-const listItems = props.items.map((item, i) => <li key={i}>{item.name}: ${item.price.toFixed(2)}</li>);
-    return <ul>{listItems}</ul>
-};
+    const listItems = props.items.map((el, i) => <li key={i}>{el.name}: ${el.price}</li>);
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+const Employee = props => {
+    return (
+        <ul>
+            <li>{props.name}</li>
+            <li>{props.age}</li>
+            <li>{props.position}</li>
+        </ul>
+    );
+}
+
+Employee.PropTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    position: PropTypes.string
+}
+
+Employee.defaultProps = {
+    position: 'employee'
+}
 
 const App = props => {
-    //think of items as something passed into the state
     const items = [
         {name: 'bread', price: 2.00},
         {name: 'milk', price: 2.50},
@@ -17,8 +49,16 @@ const App = props => {
         {name: 'tea', price: 2.00},
         {name: 'liquor', price: 10.00}
     ];
-    return <List items={items}/>
-};
+    return(
+        <div>
+            <Employee name="Sophia" age={14} />
+            <GroceryList item1="guitar pic" item2="Stratocaster" item3="drumsticks"/>
+            <List items={items} />
+        </div>
+        
+    )
+}
+
 
 ReactDOM.render(
 <App />,
