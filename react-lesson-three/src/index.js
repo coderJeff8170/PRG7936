ffeac28ae3f6d8150baa1947cb2dbf233dec0080
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-11 13:43:06 -0400

    dynamic rendering collection of objects

--- react-lesson-three/src/index.js
+++ react-lesson-three/src/index.js
@@ -4,13 +4,19 @@ import ReactDOM from 'react-dom';
 
 //okay now make this work with the map function!
 const List = props => {
-    const listItems = props.items.map((item, i) => <li key={i}>{item}</li>);
+const listItems = props.items.map((item, i) => <li key={i}>{item.name}: ${item.price.toFixed(2)}</li>);
     return <ul>{listItems}</ul>
 };
 
 const App = props => {
     //think of items as something passed into the state
-    const items = ['bread', 'milk', 'eggs', 'tea', 'liquor'];
+    const items = [
+        {name: 'bread', price: 2.00},
+        {name: 'milk', price: 2.50},
+        {name: 'eggs', price: 3.00},
+        {name: 'tea', price: 2.00},
+        {name: 'liquor', price: 10.00}
+    ];
     return <List items={items}/>
 };
 
