37ad2e9c2de5b0b4d2b046ef085e65d442f395da
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-11 23:42:10 -0400

    Hands on lesson three done dynamic rendering of components with propType checking and conditional rendering

--- L03 HandsOn/src/index.js
+++ L03 HandsOn/src/index.js
@@ -1,1 +1,35 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+// List component to render an unordered list populated by an array passed into props
+const List = props => {
+    if(!Array.isArray(props.cars)){
+        return (
+            <div>
+                <p>Sorry, I can't map {props.cars}, because it's not in an array</p>
+                <p>Please make you've supplied an array of cars!</p>
+            </div>
+        );
+    }else{
+        const cars = props.cars.map((car, i) => <li key={i}>{car}</li>);
+        return (
+            <ul>{cars}</ul>
+        );
+    }
+}
+
+List.propTypes = {
+    cars: PropTypes.array.isRequired
+}
+
+// App component to return const favoriteCars... through a list component
+const App = props => {
+    const favoriteCars = ['Honda CR-V ', 'Toyota Highlander', 'Ford F-150', 'BMW X3', 'Subaru Crosstrek'];
+    return (
+    <List cars={favoriteCars} />
+    );
+}
+
+//render App component to DOM
+ReactDOM.render(<App />, document.getElementById('root'));
--- Lesson Template/src/index.js
+++ Lesson Template/src/index.js
@@ -1,1 +1,19 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = props => {
+    return (
+    <h1>I'm {props.dogname}, a poop goat!</h1>
+    )
+}
+
+App.propTypes = {
+    dogname: PropTypes.string.isRequired
+}
+
+App.defaultProps = {
+    dogname: 'Nilla'
+}
+
+ReactDOM.render(<App dogname="Marley"/>, document.getElementById('root'));
