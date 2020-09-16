46d56543d59989a5672eb9aefb262c13ef5f29de
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-16 08:30:03 -0400

    learning routing

---
+++ react-lesson-five/src/screens/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const About = () => {
+    <Header title="About"/>
+};
+
+export default About;
---
+++ react-lesson-five/src/screens/Topics.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Topics = () => {
+    <About title="Topics"/>
+};
+
+export default Topics;
