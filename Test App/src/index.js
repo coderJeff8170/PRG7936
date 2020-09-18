f721abc1ad3fd464f48a68437fbb22257e091760
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-18 18:05:24 -0400

    Demo for binding added to Test App

--- Test App/src/index.js
+++ Test App/src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-
-const Header = props => 
-        <div>Hello World</div>
 
 
-// const App = props => {
-//     return (
-//     <h1>I'm {props.dogname}, a poop goat!</h1>
-//     )
-// }
+class MyComponent extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            myState: 'Hello World!'
+        }
+
+        this.myMethod = this.myMethod.bind(this);
+    }
+
+    myMethod(){
+        
+        this.setState({
+            myState: 'Hello Jeff!'
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>{this.state.myState}</h1>
+                <button onClick={this.myMethod}>Click Me</button>
+            </div>
+        );
+    }
+}
 
 
-ReactDOM.render(<Header />, document.getElementById('root'));
+
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'));
