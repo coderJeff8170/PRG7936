8d9e41c0e6391ce57c14a4f1a4493abe5f5ab8a8
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-22 12:12:15 -0400

    hands on 06 done

--- L06 HandsOn/src/index.js
+++ L06 HandsOn/src/index.js
@@ -1,6 +1,6 @@
-/*
-Step 1
-Convert the given JavaScript into ES6 syntax using an arrow function and filter.
+/* ---------------------------------  Step 1 -------------------------------- */
+
+//Convert the given JavaScript into ES6 syntax using an arrow function and filter. [X]
 
 const arr = [
     {"name":"chevy", "count": 2},
@@ -9,29 +9,45 @@ const arr = [
     {"name":"honda", "count": 16},
 ];
 
-const newArr = [];
+//const newArr = [];
 
-for(let i= 0; i < arr.length; i++){
-    if(arr[i].name === "ford" ){
-        newArr.push(arr[i]);
-    }
-}
+// for(let i= 0; i < arr.length; i++){
+//     if(arr[i].name === "ford" ){
+//         newArr.push(arr[i]);
+//     }
+// }
+
+//console.log("Filter results:", newArr);
+
+const newArr = arr.filter(item => item.name==="ford");
 
 console.log("Filter results:", newArr);
-Step 2
-Given the function, use the ES6 that was covered to combine the two arrays.
 
-const arr = ['Bill', 'Joe', 'Emily', 'Andrea']
+/* ---------------------------------  Step 2 -------------------------------- */
+
+// Given the function, use the ES6 that was covered to combine the two arrays. [X]
+// ('arr' renamed to 'students' to avoid conflict with step 1 in console)
+
+const students = ['Bill', 'Joe', 'Emily', 'Andrea']
 const newStudents = ['Andrew', 'Tasha', 'Carol', 'George'];
 
-function addNewStudent(array, newArray) {
-    for(let i = 0; i < newArray.length; i++) {
-        array.push(newArray[i]);
-    }
-}
+// function addNewStudent(array, newArray) {
+//     for(let i = 0; i < newArray.length; i++) {
+//         array.push(newArray[i]);
+//     }
+// }
 
-addNewStudent(arr, newStudents);
-Tip!
-You will need to use the spread operator!
+// addNewStudent(arr, newStudents);
 
-*/
+// function addNewStudent(array, newArray) {
+// return [...array, ...newArray];
+// }
+//const allStudents = addNewStudent(students, newStudents);
+
+        // ^^ ok....
+
+const allStudents = [...students, ...newStudents];
+
+        // better!
+
+console.log(allStudents);
