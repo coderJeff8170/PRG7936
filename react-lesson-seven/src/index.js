1413f450db39102fbec01bb7057d90e09433ae2a
Jeff Andrews <jeff@paintedbyjeff.com>
2020-09-23 11:28:34 -0400

    adds store and provider to index.js

--- react-lesson-seven/src/index.js
+++ react-lesson-seven/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore();
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     <Counter />
-  </div>
+  </Provider>
 );
 
 render(<App />, document.getElementById('root'));
