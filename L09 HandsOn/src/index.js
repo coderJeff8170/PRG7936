f77544d0f8cb859a2160c40bedb9b9411b002f68
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-07 21:36:46 -0400

    initial setup with state, provider and reducer

--- L09 HandsOn/src/index.js
+++ L09 HandsOn/src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/photos';
+import App from './components/App';
 
-const App = props => {
-    return (
-    <h1>I'm {props.dogname}, a poop goat!</h1>
-    )
-}
 
-App.propTypes = {
-    dogname: PropTypes.string.isRequired
-}
+/*
+Requirements
+For this project, you will using an API available with some fake data. Use the below API to list out the first 50 pictures in this data set onto your webpage.
 
-App.defaultProps = {
-    dogname: 'Nilla'
-}
+API = 'https://jsonplaceholder.typicode.com/photos'
 
-ReactDOM.render(<App dogname="Marley"/>, document.getElementById('root'));
+Step 1
+Be sure to include the following:
+
+A component
+The state
+A componentDidMount() method
+Anything else that helps render the information on the page
+SAR - Store, Action, Reducer!
+*/
+const store = createStore(reducer);
+
+
+
+
+render(
+    <Provider>
+        <App />
+    </Provider>,
+     document.getElementById('root'));
