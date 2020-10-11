fc911b77cb311c4e3e47a5ecaef126f8b527a332
Jeff Andrews <jeff@paintedbyjeff.com>
2020-10-10 21:33:27 -0400

    finished lesson 9 hands on

--- L09 HandsOnv2/src/components/Pictures.js
+++ L09 HandsOnv2/src/components/Pictures.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-const Pictures = ({photos, test}) => {
+// component renders pic thumbnails as links to larger pics
+const Pictures = ({ photos }) => {
     // const displayPics = test;
     const displayPics = photos.map(pic => <a key={pic.id} href={pic.url} target="_blank"><img src={pic.thumbnailUrl} alt={pic.title}></img></a>);
     return(
--- L09 HandsOnv2/src/index.js
+++ L09 HandsOnv2/src/index.js
@@ -2,24 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Pictures from './components/Pictures';
 
+/*
+Requirements
+For this project, you will using an API available with some fake data. Use the below API to list out the first 50 pictures in this data set onto your webpage. [X]
+
+API = 'https://jsonplaceholder.typicode.com/photos'
+
+Step 1
+Be sure to include the following:
+
+A component [X]
+The state [X]
+A componentDidMount() method [X]
+Anything else that helps render the information on the page [X]
+*/
+
 
 class App extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-            photos: [],
-            test: 'test works!'
+            photos: []
         }
     }
-
+    //method to funnel data to state once received
     onReceiveData = (data) => {
         const truncatedData = data.splice(0, 50);
         this.setState({
             photos: truncatedData
         })
     }
-
+    //method to fetch data from API endpoint
     onLoadPics = () => {
         fetch('https://jsonplaceholder.typicode.com/photos')
             .then(response => response.json())
@@ -34,7 +48,7 @@ class App extends React.Component {
             )))
             .then(data => this.onReceiveData(data))
     }
-
+    //componentDidMount ideal place to make API call, according to React docs..
     componentDidMount(){
         console.log('component mounted!');
         this.onLoadPics();
@@ -43,9 +57,9 @@ class App extends React.Component {
 
     render(){
         return (
-            <div>
-                <h1>Working!</h1>
-                <Pictures photos={this.state.photos} test={this.state.test} />
+            <div style={{textAlign: "center"}}>
+                <h1>Lesson 9 Hands On</h1>
+                <Pictures photos={this.state.photos} />
             </div>
             );
     }
